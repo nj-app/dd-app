172fe1ce0daf4b572edbcf155f3ce892c2b787ea
Sara Cassidy <saracassidy@Saras-MacBook-Pro-5.local>
2019-07-27 11:13:00 -0700

    Initial iOS project

---
+++ App1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  App1
+//
+//  Created by Sara Cassidy on 7/25/19.
+//  Copyright © 2019 Sara Cassidy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
